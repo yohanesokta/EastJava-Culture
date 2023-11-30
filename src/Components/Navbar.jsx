7a9c558c4a112e7a0bd6373e3610c28e7b63537f
yohanesokta <yohanesoktayt72@gmail.com>
2023-11-29 17:45:42 -0800

    + res nav(1330,970,880 PX)

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -20,15 +20,18 @@ const Navbar = () => {
     <>
       <nav className="">
         <img src={Logo} alt="logo" />
+          <div className="btn-dropdown">
+              <Gmaterial name="segment" />
+          </div>
         <div className="col-nav">
-            <a href="#" ><p>Home</p> <Gmaterial hide="yes" name="fiber_manual_record" /></a>
-            <a href="#" ><p>Budaya</p> <Gmaterial name="arrow_drop_down" />
+            <a href="#" className="nav-menu" ><p>Home</p> <Gmaterial hide="yes" name="fiber_manual_record" /></a>
+            <a href="#" className="nav-menu" ><p>Budaya</p> <Gmaterial name="arrow_drop_down" />
                 <DropdownMenuBudaya />
             </a>
-            <a href="#"><p>Kesenian</p>  <Gmaterial name="arrow_drop_down" />
+            <a href="#" className="nav-menu" ><p>Kesenian</p>  <Gmaterial name="arrow_drop_down" />
                 <DropdownMenuBudaya />
             </a>
-            <a href="#"><p>Kuliner</p> <Gmaterial hide="yes" name="fiber_manual_record" /></a>
+            <a href="#" className="nav-menu" ><p>Kuliner</p> <Gmaterial hide="yes" name="fiber_manual_record" /></a>
         </div>
         <div className="search">    
             <Gmaterial name="search"/>
