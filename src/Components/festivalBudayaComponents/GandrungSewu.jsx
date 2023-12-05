f3711aa85d04bff5c1d815cfd8cb23665e1cb127
yohanesokta <yohanesoktayt72@gmail.com>
2023-12-06 00:53:58 +0700

    4 festival + responsive

--- src/Components/festivalBudayaComponents/GandrungSewu.jsx
+++ src/Components/festivalBudayaComponents/GandrungSewu.jsx
@@ -1,16 +1,17 @@
-import "../../style/componets_style/festivalBudaya/gandrung-sewu.css"
-import Ornamen from "../../assets/ornament/Ornament79.svg"
-import Image from "../../assets/Festival/GandrungSewu.png"
-import Union from "../../assets/ornament/Union.svg"
-const GandrungSewu = () =>{
-    return(
+import "../../style/componets_style/festivalBudaya/gandrung-sewu.css";
+import Ornamen from "../../assets/ornament/Ornament79.svg";
+import Image from "../../assets/Festival/GandrungSewu.png";
+import Union from "../../assets/ornament/Union.svg";
+
+const GandrungSewu = () => {
+    return (
         <>
             <div className="gandrung">
                 <div className="col-top">
                     <div className="ornament">
                         <img src={Ornamen} alt="ornamen" />
                     </div>
-                    <img src={Union} alt="" id="onion-svg"/>
+                    <img src={Union} alt="" id="onion-svg" />
                     <div className="text-top">
                         <h2>2</h2>
                         <div className="line"></div>
@@ -27,14 +28,31 @@ const GandrungSewu = () =>{
                         <img src={Image} alt="Images" />
                     </div>
                     <div className="bottom-text-left">
-                        <p>Festival Tari Gandrung Sewu di Banyuwangi adalah persembahan tarian tradisional yang megah dan memukau.</p>
+                        <p>
+                            Festival Tari Gandrung Sewu di Banyuwangi adalah
+                            persembahan tarian tradisional yang megah dan
+                            memukau.
+                        </p>
                     </div>
                 </div>
                 <div className="col-very-bottom">
-                    <p>Dikenal sebagai "Sewu" yang berarti seribu, festival ini menampilkan ribuan penari Gandrung dalam harmoni gerakan yang memesona.</p>
+                    <p>
+                        Dikenal sebagai "Sewu" yang berarti seribu, festival ini
+                        menampilkan ribuan penari Gandrung dalam harmoni gerakan
+                        yang memesona.
+                    </p>
+                </div>
+                <div className="text-mobile">
+                    <p>
+                            Festival Tari Gandrung Sewu di Banyuwangi adalah
+                            persembahan tarian tradisional yang megah dan
+                            memukau. Dikenal sebagai "Sewu" yang berarti seribu, festival ini
+                        menampilkan ribuan penari Gandrung dalam harmoni gerakan
+                        yang memesona.
+                    </p>
                 </div>
             </div>
         </>
-    )
-}
-export default GandrungSewu
+    );
+};
+export default GandrungSewu;
