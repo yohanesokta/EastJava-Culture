7957f301deeb89e3d2c3d10e206e581f1b559c92
krismaliacc <pujiservic@gmail.com>
2023-12-06 15:50:27 +0800

    kuliner lokal

--- src/Components/festivalBudayaComponents/JemberFestival.jsx
+++ src/Components/festivalBudayaComponents/JemberFestival.jsx
@@ -1,5 +1,5 @@
 import "../../style/componets_style/festivalBudaya/jember-fest.css"
-
+import img from "../../assets/Festival/jember.png"
 export default function JemberFestival(){
 return(<>
     <div className="jember-fest">
@@ -9,8 +9,12 @@ return(<>
                     <h1>Jember Fashion Carnaval</h1>
                     <h2>Jember</h2>
                     <p>Jember Fashion Carnival di Jawa Timur adalah panggung megah di mana mode bertemu seni pertunjukan. Merupakan peristiwa tahunan yang menghipnotis, carnival ini menampilkan busana spektakuler dengan ornamen kreatif dan konsep visual yang luar biasa.</p>
+                </div> 
                 </div>
+            <div className="kiri">
+                <img src={img} alt="" />
             </div>
+            
     </div>
 </>)
 }
