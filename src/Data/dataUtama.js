b2696032877ab6887412ef34309e7dc4c4b2c5e3
krismaliacc <pujiservic@gmail.com>
2023-12-07 17:00:41 +0800

    kerajinan tangan

---
+++ src/Data/dataUtama.js
@@ -0,0 +1,16 @@
+import destinasiBudaya from "./destinasibudaya";
+import kesenianTradisional from "./kesenianTradisional";
+import kerajinanTangan from "./kerajinanTangan";
+import kulinerLokal from "./kulinerLokal";
+import festivalBudaya from "./festivalBudaya";
+import gayaHidup from "./gayaHidup";
+const Data = {
+  Budaya: [
+    destinasiBudaya,
+    kesenianTradisional,
+    kerajinanTangan,
+    kulinerLokal,
+    festivalBudaya,
+    gayaHidup,
+    ],
+};
---
+++ src/Data/kulinerLokal.js
@@ -0,0 +1,138 @@
+const kulinerLokal=
+{
+    "Kuliner Lokal": [
+      {
+        id: "1",
+        nama: "Rujak Cingur",
+        desc: "Rujak Cingur adalah hidangan khas Surabaya berisi mentimun, tauge, kentang, tempe, tahu, dan cingur (moncong sapi) dengan saus bumbu kacang yang lezat.",
+        loc: "Kota Surabaya",
+      },
+      {
+        id: "2",
+        nama: "Lontong Balap",
+        desc: "Lontong Balap adalah hidangan khas Surabaya yang terdiri dari potongan lontong (nasi ketan kukus), taoge (kecambah kacang hijau), tahu goreng, lentho (perkedel kacang hijau), dan potongan kerupuk, disiram dengan kuah kacang yang kaya rasa. Hidangan ini sering ditaburi bawang goreng dan sambal, memberikan perpaduan tekstur dan rasa yang unik.",
+        loc: "Kota Surabaya",
+      },
+      {
+        id: "3",
+        nama: "Tahu Telur",
+        desc: "Tahu Telur adalah hidangan khas Surabaya yang terbuat dari tahu yang digoreng, kemudian dicampur dengan adonan telur dan dimasak hingga berbentuk dadar. Hidangan ini disajikan dengan kuah kental manis yang terbuat dari bumbu kacang, petis (saus udang hitam), dan potongan daun bawang serta irisan timun.",
+        loc: "Kota Surabaya",
+      },
+      {
+        id: "4",
+        nama: "Wajik Kletik",
+        desc: "Wajik Kletik adalah makanan khas dari Blitar yang terbuat dari ketan yang dimasak dengan gula kelapa, santan, dan beberapa rempah tradisional. Makanan ini memiliki tekstur kenyal dan manis, seringkali dibentuk seperti wajik (segi enam) sebelum dipotong menjadi potongan-potongan kecil. Biasanya disajikan sebagai camilan atau hidangan manis di Blitar.",
+        loc: "Kota Blitar",
+      },
+      {
+        id: "5",
+        nama: "Getuk Lindri",
+        desc: "Getuk lindri adalah makanan khas dari Kediri yang terbuat dari ubi kayu yang direbus, dihaluskan, dan dicampur dengan gula, kemudian dibentuk menjadi silinder kecil. Getuk lindri biasanya dilapis dengan tepung kelapa atau beras untuk memberi tekstur dan rasa yang khas. Makanan ini memiliki cita rasa manis dan tekstur yang lembut, sering kali dihidangkan sebagai camilan atau makanan ringan.",
+        loc: "Kota Kediri",
+      },
+      {
+        id: "6",
+        nama: "Pecel madiun",
+        desc: "Pecel Madiun adalah hidangan sayur dengan sambal kacang gurih dan pedas, disajikan dengan rempeyek, tahu, tempe, dan lontong.",
+        loc: "Kota Madiun",
+      },
+      {
+        id: "5",
+        nama: "Getuk Lindri",
+        desc: "Getuk lindri adalah makanan khas dari Kediri yang terbuat dari ubi kayu yang direbus, dihaluskan, dan dicampur dengan gula, kemudian dibentuk menjadi silinder kecil. Getuk lindri biasanya dilapis dengan tepung kelapa atau beras untuk memberi tekstur dan rasa yang khas. Makanan ini memiliki cita rasa manis dan tekstur yang lembut, sering kali dihidangkan sebagai camilan atau makanan ringan.",
+        loc: "Kota Kediri",
+      },
+      {
+        id: "6",
+        nama: "Pecel madiun",
+        desc: "Pecel Madiun adalah hidangan sayur dengan sambal kacang gurih dan pedas, disajikan dengan rempeyek, tahu, tempe, dan lontong.",
+        loc: "Kota Madiun",
+      },
+      {
+        id: "7",
+        nama: "Bakso malang",
+        desc: "Bakso Malang: Bakso khas dari Malang, Jawa Timur. Biasanya berukuran besar, dengan daging sapi cincang dan tekstur kenyal. Disajikan dalam kuah kaldu sapi kaya rasa, dengan mie, tahu, pangsit, dan sayuran. Sajian ini sering disantap dengan sambal dan bawang goreng.",
+        loc: "Kota Malang",
+      },
+      {
+        id: "8",
+        nama: "Lumpia Kota Batu",
+        desc: "Lumpia Kota Batu adalah kuliner khas Kota Batu, Jawa Timur. Ini adalah lumpia dengan isi rebung, sayuran segar, dan kadang-kadang tambahan daging ayam atau udang. Biasanya disajikan dengan saus kacang atau saus pedas untuk menambah cita rasa. Lumpia Kota Batu memiliki cita rasa yang khas dan menjadi hidangan favorit di daerah tersebut.",
+        loc: "Kota Batu",
+      },
+      {
+        id: "9",
+        nama: "Sate Blitar",
+        desc: "Sate Blitar merupakan Sate khas dari Blitar, Jawa Timur. Daging ayam, kambing, atau sapi dipanggang dan disajikan dengan bumbu kacang atau kecap khas Blitar.",
+        loc: "Kota Blitar",
+      },
+      {
+        id: "10",
+        nama: "Tape Bondowoso",
+        desc: "Tape Bondowoso adalah makanan fermentasi khas dari Bondowoso, Jawa Timur. Ini terbuat dari fermentasi beras atau ketan yang menghasilkan rasa manis dan sedikit asam. Tape ini umumnya dimakan sebagai camilan atau bisa juga dijadikan bahan untuk membuat aneka kue tradisional.",
+        loc: "Kabupaten Bondowoso",
+      },
+      {
+        id: "11",
+        nama: "Nasi Tempong",
+        desc: "Nasi Tempong Jember adalah hidangan pedas khas dari Jember, Jawa Timur. Terdiri dari nasi panas yang disajikan dengan sambal pedas yang terbuat dari campuran cabai, terasi, dan bawang. Hidangan ini biasanya disertai dengan lauk tambahan seperti ayam goreng, ikan, atau telur.",
+        loc: "Kabupaten Jember ",
+      },
+      {
+        id: "12",
+        nama: "Soto Lamongan",
+        desc: "Soto Lamongan adalah kuliner khas Kabupaten Lamongan, Jawa Timur, yang terkenal dengan kuah kuningnya yang gurih. Soto ini biasanya disajikan dengan suwiran daging ayam, irisan kol, tomat, daun bawang, mie bihun, dan irisan telur ayam. Ciri khasnya adalah ditambahkannya serbuk koya yang terbuat dari kerupuk udang dan bawang putih goreng yang ditumbuk hingga halus. ",
+        loc: "Kabupaten Lamongan",
+      },
+      {
+        id: "13",
+        nama: "Bebek Sinjay",
+        desc: "Bebek Sinjay adalah hidangan bebek panggang khas dari Lumajang, Jawa Timur. Bebek ini disajikan dengan bumbu rempah yang khas, memberikan cita rasa yang gurih dan sedikit pedas. Biasanya disantap dengan nasi hangat dan sambal untuk menambah cita rasa khasnya.",
+        loc: "Kabupaten Lumajang",
+      },
+      {
+        id: "14",
+        nama: "Tahu kuning",
+        desc: "Tahu Kuning Kediri adalah hidangan khas dari Kediri, Jawa Timur. Ini adalah tahu yang digoreng dengan kulitnya yang kuning keemasan, diberi rasa gurih dan sedikit manis. Tahu ini biasanya disajikan dengan saus kacang atau sambal sebagai pelengkap, memberikan cita rasa yang khas dan lezat.",
+        loc: "Kabupaten Kediri",
+      },
+      {
+        id: "15",
+        nama: "Soto Ngas",
+        desc: "Soto Ngas merupakan soto khas dari Nganjuk, Jawa Timur, berkuah kental dengan rempah-rempah, daging ayam/sapi, tauge, ketupat/nasi, dan disajikan dengan sambal serta jeruk nipis untuk rasa gurih yang khas.",
+        loc: "Kabupaten Nganjuk",
+      },
+      {
+        id: "16",
+        nama: "Nasi Serpang",
+        desc: "Nasi Serpang Pamekasan: hidangan khas dari Pamekasan, Madura, berisi nasi disajikan dengan ikan laut goreng kering dan sambal petis, memberikan cita rasa gurih dan pedas khas daerah tersebut.",
+        loc: "Kabupaten Pamekasan",
+      },
+      {
+        id: "17",
+        nama: "Soto Pacitan",
+        desc: "Soto Pacitan merupakan hidangan khas dari Pacitan, Jawa Timur, berkuah kental dengan rempah-rempah, daging ayam/sapi, tauge, dan ketupat/nasi, disajikan dengan sambal dan jeruk nipis untuk cita rasa gurih yang khas.",
+        loc: "Kabupaten Pacitan",
+      },
+      {
+        id: "18",
+        nama: "Rujak Soto",
+        desc: "Rujak Soto adalah hidangan khas dari Situbondo, Jawa Timur. Ini adalah perpaduan unik antara rujak dan soto. Berisi potongan buah segar seperti nanas, bengkoang, serta tauge dan potongan ketupat, disiram dengan kuah soto yang kaya rempah-rempah. Rujak ini memiliki cita rasa segar, pedas, dan gurih yang menciptakan sensasi unik dalam satu hidangan.",
+        loc: "Kabupaten Situbondo",
+      },
+      {
+        id: "19",
+        nama: "Sate Ambal",
+        desc: "Sate Ambal adalah hidangan sate khas Tuban, Jawa Timur, terbuat dari potongan daging sapi atau kambing yang ditusuk dan dipanggang, disajikan dengan bumbu kacang yang khas dan rempah-rempah untuk cita rasa yang kaya dan unik.",
+        loc: "Kabupaten Tuban",
+      },
+      {
+        id: "20",
+        nama: "Nasi Krawu",
+        desc: "Nasi Krawu, kuliner khas Tulungagung, adalah nasi dengan potongan daging sapi iris tipis disajikan bersama sambal, irisan mentimun, dan lalapan. Hidangan ini menggambarkan cita rasa khas dengan kombinasi nasi dan daging sapi yang lezat.",
+        loc: "Kabupaten Tulungagung ",
+      },
+    ],
+  }
+  export default kulinerLokal
