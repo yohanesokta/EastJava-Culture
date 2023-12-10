9570587b972a16404ea82cf071273096f5d0eb04
krismaliacc <pujiservic@gmail.com>
2023-12-10 17:17:34 +0800

    lokasi

--- src/Data/destinasiBudaya.jsx
+++ src/Data/destinasiBudaya.jsx
@@ -4,148 +4,162 @@ const destinasiBudaya = {
       id: "1",
       nama: "Monumen Tugu Pahlawan",
       desc: "Monumen Tugu Pahlawan adalah landmark tinggi di Surabaya, Indonesia, untuk menghormati pahlawan yang berjuang untuk kemerdekaan. Terdiri dari struktur tinggi dengan api abadi di puncaknya, menjadi simbol keberanian dan pengorbanan para pejuang.",
-      Kota : "Surabaya",
+      kota : "Surabaya",
       loc: "Jl. Pahlawan, Alun-alun Contong, Kec. Bubutan, Surabaya, Jawa Timur 60174",
       MapId : "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3957.958572578533!2d112.73528777491435!3d-7.245554992760864!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd7f93fc001bc5d%3A0x110e38d8a6c23fbe!2sMonumen%20Tugu%20Pahlawan%20dan%20Museum%20Sepuluh%20Nopember%20Surabaya!5e0!3m2!1sen!2sid!4v1702190328714!5m2!1sen!2sid"
     },
     {
       id: "2",
-      Kota : "Surabaya",
+      kota : "Surabaya",
       nama: "Museum Sepuluh November",
       desc: "Museum 10 November di Surabaya adalah tempat yang mengenang pertempuran bersejarah pada 10 November 1945. Di sini, Anda bisa melihat artefak dan dokumentasi mengenai perjuangan heroik rakyat Surabaya dalam menghadapi penjajah.",
       loc: "Jl. Pahlawan, Alun-alun Contong, Kec. Bubutan, Surabaya, Jawa Timur 60174",
+      MapId:"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3957.9586193159794!2d112.7352824115889!3d-7.24554967114198!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd7f93fc001bc5d%3A0x110e38d8a6c23fbe!2sMonumen%20Tugu%20Pahlawan%20dan%20Museum%20Sepuluh%20Nopember%20Surabaya!5e0!3m2!1sid!2sid!4v1702195491238!5m2!1sid!2sid"
     },
     {
       id: "3",
+      kota:"Blitar",
       nama: "Monumen Trisula",
       desc: "Monumen Trisula di Blitar adalah monumen yang didedikasikan untuk mengenang Soekarno, Mohammad Hatta, dan Kyai Abdul Wahid Hasyim dalam perjuangan kemerdekaan Indonesia. Bentuknya menyerupai trisula dan menjadi simbol penting dalam sejarah perjuangan bangsa.",
       loc: "P3RF+5MX, Jl. Trisula, Bakung Utara, Bakung, Kec. Bakung, Kabupaten Blitar, Jawa Timur 66163",
+      MapId:"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3948.434185087698!2d112.0716574116012!3d-8.259506882957146!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e78c36ad42f91a1%3A0x4508a7083e7c5a85!2sMonumen%20Trisula!5e0!3m2!1sid!2sid!4v1702195248256!5m2!1sid!2sid"
     },
     {
       id: "4",
+      kota:"Blitar",
       nama: "Candi Penataran",
       desc: "Candi Penataran di Blitar, Jawa Timur, adalah kompleks candi Hindu terbesar di Indonesia Timur. Dengan arsitektur megah dan relief menggambarkan kisah epik Hindu, candi ini menarik bagi pengunjung yang ingin menjelajahi warisan budaya Indonesia.",
       loc: "Penataran, Kec. Nglegok, Kabupaten Blitar, Jawa Timur 66181",
+      MapId:"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3950.830290061315!2d112.2071725115981!3d-8.016430079972134!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e78f299e69b8cd1%3A0x52eb091bf02148b8!2sCandi%20Penataran!5e0!3m2!1sid!2sid!4v1702195757249!5m2!1sid!2sid"
     },
     {
       id: "5",
+      kota:"Blitar",
+      MapId:"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3950.0730690072546!2d112.17457834035272!3d-8.094031291968202!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e78ec5d3fffffff%3A0xe47a8f71c8b7aa41!2sMonumen%20Daidan%20PETA%20Blitar!5e0!3m2!1sid!2sid!4v1702195979369!5m2!1sid!2sid",
       nama: "Monumen Peta Blitar",
       desc: "Monumen Peta Blitar adalah monumen yang menampilkan peta Blitar, Jawa Timur, yang terbuat dari batu. Berlokasi di alun-alun kota, monumen ini menjadi landmark penting yang memberikan informasi geografis dan menjadi tempat berkumpul warga setempat serta pengunjung.",
       loc: "W54G+9VJ, Jl. Sudanco Supriyadi, Bendogerit, Kec. Sananwetan, Kota Blitar, Jawa Timur 66133",
     },
     {
       id: "6",
+      kota:"Mojokerto",
       nama: "Candi Brahu",
       desc: "Candi Brahu di Mojokerto adalah situs arkeologi kuno dengan arsitektur Hindu yang memukau. Meski ukurannya tidak besar, candi ini menampilkan keindahan dan detail arsitektur yang menarik bagi pengunjung yang tertarik pada sejarah klasik.",
       loc: "Kota Mojokerto",
+      MapId:"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3955.293353981631!2d112.37187224034814!3d-7.5429524925018345!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e786ce655555555%3A0xb1299504be8c8c76!2sCandi%20Brahu!5e0!3m2!1sid!2sid!4v1702196147899!5m2!1sid!2sid"
     },
     {
       id: "7",
+      kota:"Malang",
       nama: "Museum Brawijaya",
       desc: "Museum Brawijaya di Malang menampilkan koleksi artefak, senjata, foto, dan benda sejarah militer Indonesia, terutama seputar perjuangan kemerdekaan. Museum ini memberikan gambaran tentang sejarah dan peran pahlawan Indonesia.",
       loc: "Kota Malang",
-    },
-    {
-      id: "8",
-      nama: "Arca Wisnu",
-      desc: "Arca Wisnu di Malang adalah patung besar Lord Wisnu dalam posisi berdiri dengan atributnya seperti cakra di tangan. Patung ini adalah landmark spiritual yang menarik.",
-      loc: "Kota Batu",
+      MapId:"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3951.259290085848!2d112.61863944035173!3d-7.9721324920860175!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd628291cc584d5%3A0x44947ed5ec722b02!2sMuseum%20Brawijaya!5e0!3m2!1sid!2sid!4v1702196289522!5m2!1sid!2sid"
     },
     {
       id: "9",
+      kota:"Mojokerto",
       nama: "Candi Tikus",
       desc: "Candi Tikus di Trowulan adalah situs arkeologi dengan kolam berbentuk mirip tikus. Meskipun disebut (candi) ini sebenarnya bukan candi, melainkan struktur air atau tempat mandi raja pada masa lalu.",
       loc: "CCH3+7CM, Jalan Raya Trowulan, Jatirejo, Temon, Kec. Trowulan, Kabupaten Mojokerto, Jawa Timur 61362",
+      MapId:"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3955.0291850190074!2d112.40096214034848!3d-7.571796992473879!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e786d1fdd9add5d%3A0xad81bb21c4b5b075!2sCandi%20Tikus!5e0!3m2!1sid!2sid!4v1702196710838!5m2!1sid!2sid"
     },
     {
       id: "10",
+      kota:"Mojokerto",
       nama: "Candi Bajang Ratu",
       desc: "Candi Bajang Ratu adalah situs candi Hindu kuno yang terletak di Trowulan, memiliki arsitektur indah dengan relief-relief yang menghiasi dindingnya. Candi ini diperkirakan berasal dari abad ke-14 dan merupakan salah satu peninggalan sejarah yang penting dari masa Majapahit.",
       loc: "Jl. Candi Tikus No.9, Pelem, Temon, Kec. Trowulan, Kabupaten Mojokerto, Jawa Timur 61362",
+      MapId:"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3955.0665020352717!2d112.3961871403483!3d-7.567728992477736!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e786d09fc86984d%3A0xda8a79c97c17aa84!2sCandi%20Bajang%20Ratu!5e0!3m2!1sid!2sid!4v1702196792439!5m2!1sid!2sid"
     },
     {
       id: "11",
+      kota:"Pasuruan",
       nama: "Candi Gunung Gangsir",
       desc: "Candi Gunung Gangsir adalah kompleks candi Hindu di Trowulan, Jawa Timur, dari masa Majapahit. Meskipun sebagian rusak, sisa arsitekturnya tetap menunjukkan keindahan seni bangunan kuno.",
       loc: "CP7M+696, Jl. Dusun Prayan Jl. Kebun Candi, RT.02/RW.10, Gununggangsir, Gn. Gangsir, Kec. Beji, Pasuruan, Jawa Timur 67154",
+      MapId:"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3954.8908522828583!2d112.73089764034843!3d-7.586857992459237!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd7dc0d95555555%3A0x9fed821cfe50907c!2sCandi%20Gunung%20Gangsir!5e0!3m2!1sid!2sid!4v1702196886980!5m2!1sid!2sid"
     },
     {
       id: "12",
+      kota:"Probolinggo",
       nama: "Museum Probolinggo",
       desc: "Museum Probolinggo memajang artefak sejarah, seni, dan budaya kota. Koleksinya mencerminkan perkembangan masyarakat lokal, menawarkan wawasan tentang sejarah dan budaya Probolinggo.",
       loc: "Jl. Suroyo No.25, Tisnonegaran, Kec. Kanigaran, Kota Probolinggo, Jawa Timur 67211",
-    },
-    {
-      id: "13",
-      nama: "Candi Agung",
-      desc: "Candi Agung Gresik adalah situs candi Hindu yang bersejarah di Gresik, Jawa Timur. Meskipun sebagian besar bangunannya telah hilang, sisa-sisa fondasi dan batu-batu bata masih memberikan gambaran tentang kejayaan arsitektur kuno dan sejarah di wilayah tersebut.",
-      loc: "Kabupaten Gresik",
+      MapId:"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3953.364789728356!2d113.21317454034985!3d-7.75107769229997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd7adbdf787ebd5%3A0x5c0713b617aa9f1a!2sPemerintah%20Kota%20Probolinggo%20Museum%20Probolinggo!5e0!3m2!1sid!2sid!4v1702196949869!5m2!1sid!2sid"
     },
     {
       id: "14",
+      kota:"Jombang",
       nama: "Candi Rimbi",
       desc: "Candi Rimbi adalah peninggalan agama Hindu dari masa Klasik Majapahit. Candi ini juga sering disebut Cungkup Pulo. Nama Rimbi dikaitkan dengan nama tokoh pewayangan bernama Arimbi, isteri Werkudara atau yang lebih dikenal dengan nama Bima.",
       loc: "Ngrimbi, Kecamatan Bareng, Kabupaten Jombang, Jawa timur.",
+      MapId:"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3954.087945909291!2d112.34047664034924!3d-7.673694292374979!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e786928ffffffff%3A0x7573a1926fb251ad!2sCandi%20Rimbi!5e0!3m2!1sid!2sid!4v1702197134335!5m2!1sid!2sid"
     },
     {
       id: "15",
+      kota:"Jember",
       nama: "Museum Tembakau",
       desc: "Museum Tembakau di Jember menampilkan sejarah dan produksi tembakau di Jawa Timur. Koleksinya mencakup alat-alat tradisional dan informasi tentang peran tembakau dalam ekonomi dan budaya daerah.",
       loc: "68124, Jl. Kalimantan No.1, Krajan Timur, Sumbersari, Kec. Sumbersari, Kabupaten Jember, Jawa Timur 68121",
+      MapId:"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3949.4342015747216!2d113.71198414035324!3d-8.158933191905486!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd6944ad81946b5%3A0xdee0037fbed61b53!2sMuseum%20Tembakau%20Jember!5e0!3m2!1sid!2sid!4v1702197233978!5m2!1sid!2sid"
     },
     {
       id: "17",
-      nama: "Museum Magetan ",
-      desc: "Museum Magetan merupakan Museum yang memamerkan berbagai koleksi peninggalan sejarah dan budaya Kabupaten Magetan. Berlokasi di pusat Kota Magetan.",
+      nama: "Candi Simbatan",
+      kota:"Magetan",
+      desc: "Candi Simbatan adalah situs candi Hindu peninggalan Kerajaan Majapahit di Desa Simbatan, Mojokerto, Jawa Timur. Meski hanya tersisa beberapa bangunan, candi ini menampilkan relief-relief cerita kehidupan sehari-hari dan mitologi Hindu yang menggambarkan keindahan seni zaman dahulu.",
       loc: "Kota Magetan",
+      MapId:"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3953.915229942844!2d111.43939634034935!3d-7.692246592357033!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e79964e25bd7b3d%3A0xf204beea1519f24e!2sCandi%20Simbatan%20(Petirtaan%20Dewi%20Sri)!5e0!3m2!1sid!2sid!4v1702198681835!5m2!1sid!2sid"
     },
     {
       id: "18",
-      nama: "Museum Batik Pamekasan",
-      desc: "Museum Batik Pamekasan adalah tempat yang menampilkan ragam batik khas daerah Pamekasan, Madura. Di sini, pengunjung dapat melihat berbagai motif, teknik pembuatan tradisional, dan sejarah perkembangan batik di wilayah tersebut. Museum ini menjadi pusat untuk memahami keindahan serta nilai budaya yang terkandung dalam batik khas Pamekasan.",
+      kota:"Pamekasan",
+      nama: "Museum Mandhilaras",
+      desc: "Museum Mandhilaras di Pamekasan adalah tempat yang menampilkan warisan budaya Madura. Koleksinya meliputi artefak bersejarah, seni tradisional, dan benda berharga lain yang memperkenalkan sejarah dan kehidupan masyarakat Madura.",
       loc: "Kowel, Kec. Pamekasan, Kabupaten Pamekasan, Jawa Timur 69317",
+      MapId:"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3958.7037667392774!2d113.48001174034522!3d-7.160205692874145!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd77de39b906b37%3A0x3afb3d561e9c428!2sMuseum%20Mandhilaras%20Kabupaten%20Pamekasan!5e0!3m2!1sid!2sid!4v1702198554036!5m2!1sid!2sid"
     },
     {
       id: "19",
+      kota:"Pacitan",
       nama: "Gua Gong",
       desc: "Gua Gong di Pacitan adalah gua karst yang menakjubkan dengan stalaktit dan stalakmit yang indah. Terkenal karena keindahan alamnya, gua ini menawarkan pemandangan yang memukau dan formasi batu yang unik. Ini adalah destinasi populer untuk menikmati keajaiban alam Pacitan.",
       loc: "RXPH+6XF, Jl. Salam, Salam, Bomo, Kec. Punung, Kabupaten Pacitan, Jawa Timur 63553",
+      MapId:"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3949.3796795926496!2d110.97736684035331!3d-8.164448291900172!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7961f000000001%3A0x15ff9ab263f0d34e!2sGoa%20Gong%20Pacitan!5e0!3m2!1sid!2sid!4v1702197673444!5m2!1sid!2sid"
     },
     {
       id: "20",
+      kota:"Pasuruan",
       nama: "Candi Jawi",
       desc: "Candi Jawi di Pasuruan adalah kompleks candi Hindu-Buddha yang bersejarah dengan arsitektur yang megah. Tempat ini menawarkan pesona sejarah dan budaya yang klasik.",
       loc: "Jl. Raya Candiwates, Jawi, Candi Wates, Kec. Prigen, Pasuruan, Jawa Timur 67157",
+      MapId:"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3954.1921519845723!2d112.66734754034921!3d-7.662479392385869!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd7d9cd16f3d611%3A0xa128c63b8120cf90!2sCandi%20Jawi!5e0!3m2!1sid!2sid!4v1702197768014!5m2!1sid!2sid"
     },
     {
       id: "21",
+      kota:"Ponorogo",
       nama: "Pendopo Kabupaten Ponorogo",
       desc: "Pendopo Kabupaten Ponorogo adalah pusat kegiatan budaya dan seni di daerah ini. Tempat ini sering digunakan untuk pertunjukan seni, pameran budaya, dan acara tradisional. Ini adalah jantung kehidupan budaya di Ponorogo.",
       loc: "4FJ6+XWH, Jl. Aloon-Aloon Utara, Banyudono, Kec. Ponorogo, Kabupaten Ponorogo, Jawa Timur 63411",
+      MapId:"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3952.26251790415!2d111.45980344035081!3d-7.867574192187173!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e799f78d28e3d73%3A0xec7d01500d12629c!2sPendopo%20Kabupaten%20Ponorogo!5e0!3m2!1sid!2sid!4v1702197863214!5m2!1sid!2sid"
+
     },
     {
       id: "22",
+      kota:"Situbondo",
       nama: "Museum Balumbung",
       desc: "Museum Balumbung Situbondo adalah museum kecil yang dibangun untuk melestarikan sejarah perjuangan rakyat Situbondo melawan penjajah Belanda pada abad ke-19 di bawah pimpinan pahlawan lokal Ki Balumbung. Museum ini menyimpan koleksi berupa senjata tradisional, bendera perjuangan, patung para pejuang, dan foto zaman dahulu. Bangunan museum didesain menyerupai rumah adat Situbondo.",
       loc: "Jl. Raya Asembagus, Krajan Timur, Asembagus, Kec. Asembagus, Kabupaten Situbondo, Jawa Timur 68373",
+      MapId:"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3953.3894172118353!2d114.21616044034991!3d-7.74845499230247!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd12bc8054a24e7%3A0xb87aa7432aa3a398!2sMUSEUM%20BALUMBUNG!5e0!3m2!1sid!2sid!4v1702197993744!5m2!1sid!2sid"
     },
     {
       id: "23",
+      kota:"Mojokerto",
       nama: "Candi Brahu",
       desc: "Candi Brahu di Madiun adalah situs arkeologi kuno dengan arsitektur Hindu yang memukau. Meski ukurannya tidak besar, candi ini menampilkan keindahan dan detail arsitektur yang menarik bagi pengunjung yang tertarik pada sejarah klasik.",
       loc: "Kabupaten Mojokerto",
-    },
-    {
-      id: "29",
-      nama: "Candi Rimbi",
-      desc: "Candi Rimbi adalah peninggalan agama Hindu dari masa Klasik Majapahit. Candi ini juga sering disebut Cungkup Pulo. Nama Rimbi dikaitkan dengan nama tokoh pewayangan bernama Arimbi, isteri Werkudara atau yang lebih dikenal dengan nama Bima.",
-      loc: "Ngrimbi, Kecamatan Bareng, Kabupaten Jombang, Jawa timur.",
-    },
-    {
-      id: "32",
-      nama: "Gua Gong",
-      desc: "Gua Gong di Pacitan adalah gua karst yang menakjubkan dengan stalaktit dan stalakmit yang indah. Terkenal karena keindahan alamnya, gua ini menawarkan pemandangan yang memukau dan formasi batu yang unik. Ini adalah destinasi populer untuk menikmati keajaiban alam Pacitan.",
-      loc: "RXPH+6XF, Jl. Salam, Salam, Bomo, Kec. Punung, Kabupaten Pacitan, Jawa Timur 63553",
+      MapId:"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3955.293353981631!2d112.37187224034814!3d-7.5429524925018345!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e786ce655555555%3A0xb1299504be8c8c76!2sCandi%20Brahu!5e0!3m2!1sid!2sid!4v1702198045489!5m2!1sid!2sid"
     },
   ],
 }
