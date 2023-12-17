11c1dcb683846339bc983732f4cf58dff69851f7
yohanesokta <yohanesoktayt72@gmail.com>
2023-12-18 00:03:23 +0700

    fix destinationn click

--- src/Elements/Home/CardSlideDestinasi.jsx
+++ src/Elements/Home/CardSlideDestinasi.jsx
@@ -1,46 +1,74 @@
 import "../../css/Home/card-slide-four.css";
-import CardButton from "../../assets/HomeElements/arrow_back.svg";
-import CardElement from "../Bundle/CardElement";
-import GandrungSewu from "../../assets/HomeElements/FestivalImage/fest1.png";
-import KebayaBanyuwangi from "../../assets/HomeElements/FestivalImage/fest2.png";
-import BatikProbolingo from "../../assets/HomeElements/FestivalImage/fest3.png";
-import ReogPonorogo from "../../assets/HomeElements/FestivalImage/fest4.png";
 
-export default function CardSlideDestinasi() {
+import CardButton from "../../assets/HomeElements/arrow_back.svg";
+import CardElement from "../Bundle/CardElementDestinasi";
+import GandrungSewu from "../../assets/HomeElements/FestivalImage/fest1.png";
+import Image1 from "../../assets/Destinasi/T1.png"
+import Image2 from "../../assets/Destinasi/T14.png"
+import Image3 from "../../assets/Destinasi/T25.png"
+import Image4 from "../../assets/Destinasi/T24.png"
+import Image5 from "../../assets/Destinasi/T26.png"
+import Image6 from "../../assets/Destinasi/T12.png"
+import Image7 from "../../assets/Destinasi/T15.png"
+import Image8 from "../../assets/Destinasi/T18.png"
+
+import BtnControl from "../Bundle/BtnControl";
+
+export default function CardSlideFour() {
+
     return (
         <>
             <div className="card-title">
-                <h1 style={{color:"gray"}} >Destinasi Budaya Jawa Timur</h1>
+                <h1>Gelaran Budaya Jawa Timur</h1>
             </div>
             <div className="card-slide-four">
                 <div className="card-slide">
                     <div className="card-button">
-                        <img src={CardButton} alt="" />
-                        <img src={CardButton} alt="" />
+                        <img src={CardButton} alt="" onClick={() => { BtnControl('kanan', 8) }} />
+                        <img src={CardButton} alt="" onClick={() => { BtnControl('kiri', 8) }} />
                     </div>
-                    <div className="container-field">
-                        <div className="field-content">
+                    <div className="container-field" id="container-content">
+                        <div className="field-content" id='longkonten'>
                             <CardElement
-                                image={GandrungSewu}
-                                tanggal="22 DESEMBER 2023"
-                                tittle="FESTIVAL GANDRUNG SEWU"
+                                image={Image1}
+                                link="/destinasi/Surabaya"
+                                tittle="Kota Surabaya"
                             />
                             <CardElement
-                                image={KebayaBanyuwangi}
-                                tanggal="22 DESEMBER 2023"
-                                tittle="FESTIVAL KEBAYA BANYUWANGI"
+                                image={Image2}
+                                link={"/destinasi/Jombang"}
+                                tittle="Kota Jombang"
                             />
                             <CardElement
-                                image={BatikProbolingo}
-                                tanggal="22 DESEMBER 2023"
-                                tittle="FESTIVAL BATIK PROBOLINGGO"
+                                image={Image3}
+                                link={"/destinasi/Blitar"}
+                                tittle="Kabupaten Blitar"
                             />
                             <CardElement
-                                image={ReogPonorogo}
-                                tanggal="22 DESEMBER 2023"
-                                tittle="FESTIVAL  REOG PONOROGO"
+                                image={Image4}
+                                link={"/destinasi/Mojokerto"}
+                                tittle="Kota Madiun"
                             />
-                            
+                            <CardElement
+                                image={Image5}
+                                link={"/destinasi/Malang"}
+                                tittle="Kota Malang"
+                            />
+                            <CardElement
+                                image={Image6}
+                                link={"/destinasi/Probolinggo"}
+                                tittle="Probolinggo"
+                            />
+                            <CardElement
+                                image={Image7}
+                                link={"/destinasi/Jember"}
+                                tittle="Jember"
+                            />
+                            <CardElement
+                                image={Image8}
+                                link={"/destinasi/Pamekasan"}
+                                tittle="Pamekasan"
+                            />
                         </div>
                     </div>
                 </div>
--- src/Pages/PageDestinasi.jsx
+++ src/Pages/PageDestinasi.jsx
@@ -2,15 +2,14 @@ import ExplorasiBudaya from "../Components/Home/ExplorasiBudaya";
 import Navbar from "../Components/Navbar";
 import Footer from "../Components/bundle/Footer";
 import CardSlideDestinasi from "../Elements/Home/CardSlideDestinasi";
-import CardSlideFour from "../Elements/Home/CardSlideFour";
 
-const PageDestinasi = () =>{
-    return(<>
+const PageDestinasi = () => {
+    return (<>
         <Navbar />
-        <div className="nav-gap" style={{height:"120px"}}>
+        <div className="nav-gap" style={{ height: "120px" }}>
         </div>
-        <ExplorasiBudaya/>
-        <CardSlideFour/>
+        <ExplorasiBudaya />
+        <CardSlideDestinasi />
         <Footer />
     </>);
 }
