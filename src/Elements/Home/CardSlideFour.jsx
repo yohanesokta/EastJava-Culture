c494e48e5ed2757a41cf95cc59e4758310bda1de
yohanesokta <yohanesoktayt72@gmail.com>
2023-12-19 10:17:25 +0700

    Fix Not Comming Soon

--- src/Elements/Home/CardSlideFour.jsx
+++ src/Elements/Home/CardSlideFour.jsx
@@ -2,23 +2,21 @@ import "../../css/Home/card-slide-four.css";
 import CardButton from "../../assets/HomeElements/arrow_back.svg";
 import CardElement from "../Bundle/CardElement";
 
-
 import GandrungSewu from "../../assets/HomeElements/FestivalImage/fest1.png";
 import KebayaBanyuwangi from "../../assets/HomeElements/FestivalImage/fest2.png";
 import BatikProbolingo from "../../assets/HomeElements/FestivalImage/fest3.png";
 import ReogPonorogo from "../../assets/HomeElements/FestivalImage/fest4.png";
-import KarapanSonok from "../../assets/HomeElements/FestivalImage/KarapanSonok.png"
-import Wayang from "../../assets/HomeElements/FestivalImage/Wayang.png"
-import Pandalungan from "../../assets/HomeElements/FestivalImage/pandalungan.png"
-import Rontek from "../../assets/HomeElements/FestivalImage/rontek_pacitan.png"
-import PetikLaut from "../../assets/HomeElements/FestivalImage/PetikLaut.png"
-import KarapanSapi from "../../assets/HomeElements/FestivalImage/KarapanSapi.png"
-import Kenduren from "../../assets/HomeElements/FestivalImage/Kenduren.png"
-import JemberFasion from "../../assets/HomeElements/FestivalImage/JemberFasion.png"
+import KarapanSonok from "../../assets/HomeElements/FestivalImage/KarapanSonok.png";
+import Wayang from "../../assets/HomeElements/FestivalImage/Wayang.png";
+import Pandalungan from "../../assets/HomeElements/FestivalImage/pandalungan.png";
+import Rontek from "../../assets/HomeElements/FestivalImage/rontek_pacitan.png";
+import PetikLaut from "../../assets/HomeElements/FestivalImage/PetikLaut.png";
+import KarapanSapi from "../../assets/HomeElements/FestivalImage/KarapanSapi.png";
+import Kenduren from "../../assets/HomeElements/FestivalImage/Kenduren.png";
+import JemberFasion from "../../assets/HomeElements/FestivalImage/JemberFasion.png";
 import BtnControl from "../Bundle/BtnControl";
 
 export default function CardSlideFour() {
-
     return (
         <>
             <div className="card-title">
@@ -27,43 +25,62 @@ export default function CardSlideFour() {
             <div className="card-slide-four">
                 <div className="card-slide">
                     <div className="card-button">
-                        <img src={CardButton} alt="" onClick={() => { BtnControl('kanan', 12) }} />
-                        <img src={CardButton} alt="" onClick={() => { BtnControl('kiri', 12) }} />
+                        <img
+                            src={CardButton}
+                            alt=""
+                            onClick={() => {
+                                BtnControl("kanan", 12);
+                            }}
+                        />
+                        <img
+                            src={CardButton}
+                            alt=""
+                            onClick={() => {
+                                BtnControl("kiri", 12);
+                            }}
+                        />
                     </div>
                     <div className="container-field" id="container-content">
-                        <div className="field-content" id='longkonten'>
+                        <div className="field-content" id="longkonten">
                             <CardElement
                                 image={GandrungSewu}
-                                tanggal="22 DESEMBER 2023"
+                                tanggal="14-16 Sep 2023"
                                 red={true}
-                                link={'/ivent/kalender/'}
+                                link={"/event/kalender/GandrungSewu"}
                                 tittle="FESTIVAL GANDRUNG SEWU"
                             />
-                             <CardElement
+                            <CardElement
                                 image={Wayang}
-                                tanggal="22 DESEMBER 2023"
+                                red={true}
+                                link={"/event/kalender/Wayang"}
+                                tanggal="5 November 2023"
                                 tittle="FESTIVAL WAYANG KULIT"
                             />
-                             <CardElement
+                            <CardElement
+                                red={true}
                                 image={Rontek}
-                                tanggal="22 DESEMBER 2023"
+                                link={"/event/kalender/RontekPacitan"}
+                                tanggal="18-19 Nov 2023"
                                 tittle="FESTIVAL RONTEK PACITAN"
                             />
-                             <CardElement
+                            <CardElement
                                 image={Pandalungan}
-                                tanggal="22 DESEMBER 2023"
+                                red={true}
+                                link={"/event/kalender/Pandalungan"}
+                                tanggal="21 Oktober 2023"
                                 tittle="FESTIVAL PANDALUNGAN"
                             />
+                            {/* End 4 */}
                             <CardElement
                                 image={KebayaBanyuwangi}
-                                tanggal="22 DESEMBER 2023"
+                                tanggal="Comming Soon"
                                 red={true}
-                                link={'/ivent/Gallery'}
+                                link={"/ivent/Gallery"}
                                 tittle="FESTIVAL KEBAYA BANYUWANGI"
                             />
                             <CardElement
                                 image={BatikProbolingo}
-                                tanggal="22 DESEMBER 2023"
+                                tanggal="Comming Soon"
                                 red={true}
                                 tittle="FESTIVAL BATIK PROBOLINGGO"
                             />
@@ -76,28 +93,30 @@ export default function CardSlideFour() {
 
                             <CardElement
                                 image={KarapanSonok}
-                                tanggal="22 DESEMBER 2023"
+                                tanggal="Comming Soon"
+                                red={true}
                                 tittle="FESTIVAL SAPI SONOK MADURA"
                             />
-                           
-                           
-                           
+
                             {/* END 8 */}
                             <CardElement
+                                red={true}
                                 image={PetikLaut}
-                                tanggal="22 DESEMBER 2023"
+                                tanggal="Comming Soon"
                                 tittle="FESTIVAL PETIK 
                                 LAUT"
                             />
                             <CardElement
+                                red={true}
+                                image={Kenduren}
+                                tanggal="Comming Soon"
+                                tittle="FESTIVAL KENDUREN JOMBANG"
+                            />
+
+                            <CardElement
                                 image={KarapanSapi}
                                 tanggal="22 DESEMBER 2023"
                                 tittle="FESTIVAL KARAPAN SAPI"
-                            />
-                            <CardElement
-                                image={Kenduren}
-                                tanggal="22 DESEMBER 2023"
-                                tittle="FESTIVAL KENDUREN JOMBANG"
                             />
                             <CardElement
                                 image={JemberFasion}
--- src/Pages/IventKalender.jsx
+++ src/Pages/IventKalender.jsx
@@ -6,18 +6,28 @@ import CalenderImage from "../assets/Festival/reog-calender.png"
 import Gmaterial from "../Components/bundle/GoogleMaterial"
 import IventHero from "../Elements/Bundle/IventHero"
 import Title from "../Components/title"
+import { useParams } from "react-router-dom"
+import Datas from "../Data/event/Main"
 
-const IventKalender = () => {
+function IventKalender() {
+    let data = {}
+    
+    Datas.Data.forEach((e)=>{
+       if(e.nameId == useParams().slug){
+            data = e
+       }
+    })
+    
     return (<>
-            <Title string="East Java Culture | Ivent" />
+            <Title string={`East Java Culture | Ivent | ${data.nameId}`} />
         <Navbar />
         <div className="nav-gap"></div>
-        <IventHero title="Festival Reog Ponorogo" desc="Festival Reog Ponorogo adalah acara tahunan yang menampilkan kekayaan seni dan budaya daerah Ponorogo, terutama seni pertunjukan Reog yang menjadi ciri khasnya. Reog sendiri adalah bentuk seni pertunjukan yang melibatkan tarian, musik, dan pertunjukan topeng yang spektakuler." image={ThumnailImgae} />
+        <IventHero title={data.nama} desc={data.desc} image={`/thumb/F${data.id}.jpg`} />
 
 
-        <h2 className="title-ivent">berikut ini adalah tanggal akan terlaksananya kegiatan festival reog ponorogo</h2>
+        <h2 className="title-ivent">berikut ini adalah tanggal akan terlaksananya kegiatan {data.nama}</h2>
         <div className="calender-container">
-            <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%233F51B5&ctz=Asia%2FJakarta&showTitle=1&showNav=1&showTz=0&showCalendars=1&showTabs=0&showPrint=1&mode=MONTH" height="600" frameborder="0" scrolling="no"></iframe>
+            <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%233F51B5&ctz=Asia%2FJakarta&showTitle=1&showNav=1&showTz=0&showCalendars=1&showTabs=0&showPrint=1&mode=MONTH" height="600" frameBorder="0" scrolling="no"></iframe>
         </div>
         <div className="calender-container">
             <div className="calender-field">
@@ -26,8 +36,8 @@ const IventKalender = () => {
                         <Gmaterial name="calendar_month" />
                         <p>DATE</p>
                     </div>
-                    <p>8 September 2023</p>
-                    <p>Expired</p>
+                    <p>{data.tgl}</p>
+                    {/* <p>Expired</p> */}
                 </div>
 
                 <div className="card-calender">
@@ -35,14 +45,14 @@ const IventKalender = () => {
                         <Gmaterial name="calendar_month" />
                         <p>TIME</p>
                     </div>
-                    <p>19:30 - 23:00</p>
+                    <p>{data.time}</p>
                 </div>
                 <div className="card-calender">
                     <div className="card-icon">
                         <Gmaterial name="location_on" />
                         <p>LOCATION</p>
                     </div>
-                    <p>Desa Majapura, Kecamatan Bobotsari, Purbalingga</p>
+                    <p>{data.loc}</p>
                 </div>
             </div>
         </div>
