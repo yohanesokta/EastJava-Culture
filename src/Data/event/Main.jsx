c64d1352d59ba1fc4a24fb24c0faeb43d34aacfe
yohanesokta <yohanesoktayt72@gmail.com>
2023-12-20 04:49:59 +0700

    Update Maps

--- src/Data/event/Main.jsx
+++ src/Data/event/Main.jsx
@@ -1,44 +1,48 @@
 const MainEvent = {
-    Data:[
+    Data: [
         {
-            id:"1",
-            nama:"Festival Gandrung Sewu",
-            desc:"Festival ini melibatkan lebih dari seribu penari Gandrung dari berbagai jenjang bangku pendidikan mulai dari SD, SMP, hingga SMA. Dengan ketentuan tinggi badan minimal 140cm.",
-            tgl:"14-16 September 2023",
-            time:"18.00",
-            loc:"Pantai Boom Marina Banyuwangi",
-            geocalender:"",
-            nameId:"GandrungSewu"
+            id: "1",
+            nama: "Festival Gandrung Sewu",
+            desc: "Festival ini melibatkan lebih dari seribu penari Gandrung dari berbagai jenjang bangku pendidikan mulai dari SD, SMP, hingga SMA. Dengan ketentuan tinggi badan minimal 140cm.",
+            tgl: "14-16 September 2023",
+            time: "18.00",
+            loc: "Pantai Boom Marina Banyuwangi",
+            geocalender: "",
+            nameId: "GandrungSewu",
+            Geolocation: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7897.814195324025!2d114.37942534080317!3d-8.212097567280535!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd144d77ac222cb%3A0x843a72e0f108fbc0!2sPantai%20Boom!5e0!3m2!1sid!2sid!4v1703021446414!5m2!1sid!2sid"
         },
         {
-            id:"2",
-            nama:"Pekan Wayang Jawa Timur",
-            desc:`Tema yang diambil pada acara ini adalah "Karena Ku SuWayang: Wayang, Renungan Kepemimpinan dan Kebijaksanaan". Acara ini bertujuan untuk merestorasi nilai-nilai kebijaksanaan dan semangat rasa cinta pada wayang dan seni pedalangan`,
-            tgl:"5 November 2023",
-            time:"20.00",
-            loc:"Pendapa Jayangrana Taman Budaya Jawa Timur",
-            geocalender:"",
-            nameId:"Wayang"
+            id: "2",
+            nama: "Pekan Wayang Jawa Timur",
+            desc: `Tema yang diambil pada acara ini adalah "Karena Ku SuWayang: Wayang, Renungan Kepemimpinan dan Kebijaksanaan". Acara ini bertujuan untuk merestorasi nilai-nilai kebijaksanaan dan semangat rasa cinta pada wayang dan seni pedalangan`,
+            tgl: "5 November 2023",
+            time: "20.00",
+            loc: "Pendapa Jayangrana Taman Budaya Jawa Timur",
+            geocalender: "",
+            nameId: "Wayang",
+            Geolocation: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7915.720229154533!2d112.73033319357907!3d-7.256756999999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd7f9620aad4959%3A0xe2cdd004e045ee68!2sUPT.%20TAMAN%20BUDAYA%20JAWA%20TIMUR!5e0!3m2!1sid!2sid!4v1703022000634!5m2!1sid!2sid"
         },
         {
-            id:"3",
-            nama:"Festival Rontek Pacitan",
-            desc:"Festival ini bertujuan untuk menampilkan keindahan Rontek, kombinasi musik tradisional dan kontemporer, serta seni budaya tradisional untuk membangun tourisme di Pacitan",
-            tgl:"18-19 November 2023",
-            time:"19.00",
-            loc:"Alun-alun Pacitan",
-            geocalender:"",
-            nameId:"RontekPacitan"
+            id: "3",
+            nama: "Festival Rontek Pacitan",
+            desc: "Festival ini bertujuan untuk menampilkan keindahan Rontek, kombinasi musik tradisional dan kontemporer, serta seni budaya tradisional untuk membangun tourisme di Pacitan",
+            tgl: "18-19 November 2023",
+            time: "19.00",
+            loc: "Alun-alun Pacitan",
+            geocalender: "",
+            nameId: "RontekPacitan",
+            Geolocation: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3949.079498543414!2d111.10062487487416!3d-8.19474669183698!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7961dc85d1837b%3A0x3f19b40003c8424!2sAlun-Alun%20Pacitan!5e0!3m2!1sid!2sid!4v1703022074193!5m2!1sid!2sid"
         },
         {
-            id:"4",
-            nama:"Festival Pandalungan",
-            desc:"Festival Budaya Pandalungan tahun 2023 di Pelataran Candi Jabung Desa Jabung Candi Kecamatan Paiton Kabupaten Probolinggo pada Sabtu (21/10/2023) sore berlangsung semarak dan meriah.",
-            tgl:"21 Oktober 2023",
-            time:"15.00",
-            loc:"Pelataran Candi Jabung, Desa Jabung Candi, Kecamatan Paiton, Kabupaten Probolinggo",
-            geocalender:"",
-            nameId:"Pandalungan"
+            id: "4",
+            nama: "Festival Pandalungan",
+            desc: "Festival Budaya Pandalungan tahun 2023 di Pelataran Candi Jabung Desa Jabung Candi Kecamatan Paiton Kabupaten Probolinggo pada Sabtu (21/10/2023) sore berlangsung semarak dan meriah.",
+            tgl: "21 Oktober 2023",
+            time: "15.00",
+            loc: "Pelataran Candi Jabung, Desa Jabung Candi, Kecamatan Paiton, Kabupaten Probolinggo",
+            geocalender: "",
+            nameId: "Pandalungan",
+            Geolocation: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3953.51303491395!2d113.46914377486915!3d-7.735276992283146!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd70160580ad9d1%3A0x83ef4864691e539!2sCandi%20Jabung!5e0!3m2!1sid!2sid!4v1703022121681!5m2!1sid!2sid"
         }
     ]
 }
--- src/Pages/IventKalender.jsx
+++ src/Pages/IventKalender.jsx
@@ -1,9 +1,6 @@
 import Navbar from "../Components/Navbar"
 import "../css/Festival/ivent-kalender.css"
 import Footer from "../Components/bundle/Footer"
-import ThumnailImgae from "../assets/Festival/ponorogo-thumb.png"
-import CalenderImage from "../assets/Festival/reog-calender.png"
-import Gmaterial from "../Components/bundle/GoogleMaterial"
 import IventHero from "../Elements/Bundle/IventHero"
 import Title from "../Components/title"
 import { useParams } from "react-router-dom"
@@ -32,7 +29,7 @@ function IventKalender() {
         <div className="calender-container iframe-container">
             <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=Asia%2FJakarta&showTitle=0&showNav=1&showDate=1&showCalendars=0&showTz=0&showTabs=0&src=MmM2ZDZmNzU3OGRiMGM0NDA0YmY0YmYyOTA2YTBhZDEwMTgxMTllZTA2YWRhOWZiNzM5NzViM2EyN2E5ZTFhMkBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23C0CA33" height="600" frameborder="0" scrolling="no"></iframe>
 
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7897.814195324025!2d114.37942534080317!3d-8.212097567280535!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd144d77ac222cb%3A0x843a72e0f108fbc0!2sPantai%20Boom!5e0!3m2!1sid!2sid!4v1702963568105!5m2!1sid!2sid" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <iframe src={data.Geolocation} height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div >
         <Footer />
     </>)
